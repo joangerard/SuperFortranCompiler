0a33dd19e741d4cbde648ef141b2bf96fad8f8c9
Sebastian Gerard <joan.sebastian.gerard@gmail.com>
2018-10-10 15:03:26 +0200

    Feature: Add counter.sf file and modify runner.sh.

--- runner.sh
+++ runner.sh
@@ -1,7 +1,23 @@
-jflex src/LexicalAnalizer.flex
-javac -cp src src/LexicalAnalizer.java
-cd src
-jar cvmf META-INFO/MANIFEST.MF part1.jar *.class
-mv part1.jar ../dist/part1.jar
-cd ..
-java -jar dist/part1.jar more/inputs/$1
+#!/bin/bash
+
+if [ $# -gt 0 ]
+then 
+    
+    jflex src/LexicalAnalizer.flex
+    javac -cp src src/LexicalAnalizer.java
+    cd src
+    jar cvmf META-INFO/MANIFEST.MF part1.jar *.class
+    mv part1.jar ../dist/part1.jar
+    cd ..
+    java -jar dist/part1.jar test/$1
+
+else
+    echo "
+
+    File name param missing... Please add your .sf file to compile 
+    in the test/* directory and compile it like: 
+
+    ./runner.sh counter.sf
+    
+    "
+fi

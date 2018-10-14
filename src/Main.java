9e257304605ed9fa370989cba37cdf52a9d5993b
nik1168 <ngeisser32@gmail.com>
2018-10-14 23:19:44 +0200

    Feature: - Adding a Main.java file with a main method that executes the lexer - Adding gradle build and gradle settings files to build the project and generate a jar file - Updating README.md File - Adding README files in more and doc folders for further documentation

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main{
+
+    public static void main(String args[]){
+        
+        System.out.println("Main class");
+
+        if (args.length == 0) {
+            System.out.println("Usage : java LexicalAnalizer [ --encoding <name> ] <inputfile(s)>");
+        }
+        else {
+            int firstFilePos = 0;
+            String encodingName = "UTF-8";
+            if (args[0].equals("--encoding")) {
+                firstFilePos = 2;
+                encodingName = args[1];
+                try {
+                    java.nio.charset.Charset.forName(encodingName); // Side-effect: is encodingName valid? 
+                } catch (Exception e) {
+                    System.out.println("Invalid encoding '" + encodingName + "'");
+                    return;
+                }
+            }
+            for (int i = firstFilePos; i < args.length; i++) {
+                LexicalAnalizer scanner = null;
+                try {
+                    java.io.FileInputStream stream = new java.io.FileInputStream(args[i]);
+                    java.io.Reader reader = new java.io.InputStreamReader(stream, encodingName);
+                    scanner = new LexicalAnalizer(reader);
+                    scanner.yylex();
+                }
+                catch (java.io.FileNotFoundException e) {
+                    System.out.println("File not found : \""+args[i]+"\"");
+                }
+                catch (java.io.IOException e) {
+                    System.out.println("IO error scanning file \""+args[i]+"\"");
+                    System.out.println(e);
+                }
+                catch (Exception e) {
+                    System.out.println("Unexpected exception:");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}

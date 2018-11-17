119ae80976fc91691dd28652debcab2bd7eea610
Sebastian Gerard <joan.sebastian.gerard@gmail.com>
2018-11-17 02:19:50 +0100

    Add derivation rules to syntax checker.

---
+++ src/Rule.java
@@ -0,0 +1,16 @@
+public class Rule {
+    private Integer number;
+    private String productionRule;
+    private Symbol terminal;
+
+    public Rule(Integer number, String productionRule, Symbol terminal) {
+        this.number = number;
+        this.productionRule = productionRule;
+        this.terminal = terminal;
+    }
+
+    @Override
+    public String toString(){
+        return this.number + "\tby " + productionRule;
+    }
+}
